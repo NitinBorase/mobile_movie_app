afc0a5aea84efad2677f860ba2c0ecdded3530a3
NitinBorase <nitinborase72@gmail.com>
2026-02-23 13:49:34 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View className={"flex-1 justify-center items-center"}>
+    <SafeAreaView className={"flex-1 justify-center items-center"}>
       <Text className={"text-5xl text-blue-500 font-bold"}>Welcome!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
