ef1ef8f59eb485767687c596d2e31523eda45b6a
NitinBorase <nitinborase72@gmail.com>
2026-02-26 18:25:29 +0530

    UI change and movie fetch by search

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,6 +7,7 @@ import useFetch from "@/services/useFetch";
 import {fetchMovies} from "@/services/api";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/searchBar";
+import {updateSearchCount} from "@/services/appwrite";
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState("");
@@ -32,6 +33,11 @@ const Search = () => {
 
         return () => clearTimeout(timeOutId);
     }, [searchQuery]);
+
+    useEffect(() => {
+        if(movies?.length > 0 && movies?.[0])
+            updateSearchCount(searchQuery, movies[0]);
+    }, [movies]);
     return (
         <View className="flex-1 bg-black">
             <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover"/>
